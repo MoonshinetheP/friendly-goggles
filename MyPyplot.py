3bc7f12645dbdf39d276960b6eb68e92a65dcaba
Steven Linfield <S.Linfield@outlook.com>
2022-01-19 14:34:37 +0100

    Reverted main back to old GUI

--- MyPyplot.py
+++ MyPyplot.py
@@ -1,6 +1,8 @@
 """MODULES"""
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtGui as qtg
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
 
 import matplotlib as mpl 
 import matplotlib.pyplot as plt
